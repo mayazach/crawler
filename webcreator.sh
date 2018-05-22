c5a54baace549e30136ec043e927888047b3ba8c
maya <mayazaha2@gmail.com>
2018-05-22 13:01:36 +0300

    Web site creator: Check command line input

--- webcreator.sh
+++ webcreator.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+re='^[0-9]+$'
+
 if [ "$#" != 4 ]; then
 	echo "Need input parameters: root_directory text_file w p"
 	exit 1
+fi
+
+if [ ! -e "$1" ];
+then
+    echo "Root directory must exist"
+    exit 1
+fi
+
+if [ ! -e "$2" ];
+then
+    echo "Text file must exist"
+    exit 1
+fi
+
+if ! [[ "$3" =~ $re ]] ;
+then
+    echo "w must be a positive integer"
+    exit 1
+fi
+
+if ! [[ "$4" =~ $re ]] ;
+then
+    echo "p must be a positive integer"
+    exit 1
 fi
 
 echo "Hello program"
