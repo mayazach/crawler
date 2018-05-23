6d7455af538a7c16b763825c8f117e6659f34c1a
maya <mayazaha2@gmail.com>
2018-05-23 19:00:50 +0300

    Web Site Creator: Adding text

--- webcreator.sh
+++ webcreator.sh
@@ -60,12 +60,39 @@ do
     done
 done
 
+lineNum=$(cat "$2" | wc -l)
+fileNum=`expr $4 \* $3`
+
 for ((i=0; i < "$3"; i++))
 do
     mkdir "$1/site$i"
     for ((j=0; j < "$4"; j++))
     do
-	lineNum=$(cat "$2" | wc -l)
+	farray=()
+	qarray=()
+	fsize=0
+	while [ "$fsize" -lt "$f" ]
+	do
+	    temp=$((RANDOM % $4))
+	    if [ "$temp" != "$j" ];
+	    then
+		temp=`expr $minlink + $temp`
+		farray+=("$temp")
+		let "fsize=fsize+1"
+	    fi
+	done
+	qsize=0
+	minlink=`expr $4 \* $i`
+        maxlink=`expr $4 \* $i + $4 - 1`
+        while [ "$qsize" -lt "$q" ]
+        do
+            temp=$((RANDOM % $fileNum))
+            if [ "$temp" -lt "$minlink" -o "$temp" > "$maxlink" ];
+            then
+                qarray+=("$temp")
+                let "qsize=qsize+1"
+            fi
+        done
 	k=$((1 + RANDOM % $(($lineNum - 2000))))
 	m=$((1001 + RANDOM % 1000))
 	index=`expr $4 \* $i + $j`
@@ -73,7 +100,29 @@ do
 	echo "<!DOCTYPE html>" >> ${filenames[index]}
 	echo "<html>" >> ${filenames[index]}
 	echo "    <body>" >> ${filenames[index]}
+	fq=`expr $f + $q`
+	mfq=`expr $m / $fq`
+	for((i2=0;i2 < "$fq";i2++))
+	do
+	    for((j2=0;j2 < "$mfq";j2++))
+	    do
+		offset=`expr $k + $i2 \* $fq + $j2`
+		echo "    ${lines[offset]}" >> ${filenames[index]}
+	    done
+	done
+	echo "    </body>" >> ${filenames[index]}
+	echo "</html>" >> ${filenames[index]}
     done
+done
+
+for ((i=0; i<${#farray[*]}; i++));
+do
+    echo "${filenames[${farray[i]}]}"
+done
+
+for ((i=0; i<${#qarray[*]}; i++));
+do
+    echo "${filenames[${qarray[i]}]}"
 done
 
 echo "#Done"
