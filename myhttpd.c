253082499e8af91820533ebccd220bef29d461f5
mayazaha <mayazaha2@gmail.com>
2018-06-02 17:49:36 +0300

    Web server: Answer commands received in command port

--- myhttpd.c
+++ myhttpd.c
@@ -5,14 +5,20 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <sys/time.h>
+#define BUFFSIZE 256
 
 int main(int argc,char** argv){
-	int serving_port,command_port,num_threads,serving_sock,command_sock;
-	int i,found = 0;
+	int serving_port,command_port,num_threads,serving_sock,command_sock,csock;
+	int i,found = 0,pages_served = 0,bytes_served = 0,index,mins_running;
+	double seconds_running;
 	char* root_dir;
 	struct sockaddr_in server,command;
 	struct sockaddr *serverptr,*commandptr;
 	unsigned int serverlen, commandlen;
+	FILE *sock_fp;
+	char command_name[BUFFSIZE];
+	struct timeval  start, current;
 	
 	if(argc == 9){
 		for(i=1;i<(argc-1);i++){
@@ -83,6 +89,8 @@ int main(int argc,char** argv){
 		printf("Wrong number of arguments.\n");
 		return 1;
 	}
+	gettimeofday(&start, NULL);
+
 	
 	if ((serving_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		perror("server socket");
@@ -104,10 +112,56 @@ int main(int argc,char** argv){
 	commandlen = sizeof command;
 	
 	if (bind(serving_sock, serverptr, serverlen) < 0){
-		perror("server bind"); exit(-1);
+		perror("server bind"); 
+		exit(1);
 	}
 	if (bind(command_sock, commandptr, commandlen) < 0){
-		perror("command bind"); exit(-1);
+		perror("command bind"); 
+		exit(1);
+	}
+	
+	if ( listen(command_sock, 5) != 0 ){
+		perror( "listen" );
+		exit(1);
+	}
+	
+	while(1){
+		if ( (csock = accept(command_sock, NULL, NULL)) < 0 ){
+			perror("accept");
+			return 1;
+		}
+		if ((sock_fp = fdopen(csock,"r+")) == NULL){
+			perror("fdopen");
+			return 1;
+		}
+		if (fgets(command_name, BUFFSIZE, sock_fp) == NULL){
+			perror("reading command");
+			return 1;
+		}
+		index = strlen(command_name) - 1;
+		while(index > 0){
+			if((command_name[index] == '\n') || (command_name[index] == '\r')){
+				command_name[index] = '\0';
+				index--;
+			}
+			else 
+				break;
+		}
+		if(!strcmp(command_name,"SHUTDOWN"))
+			break;
+		else if(!strcmp(command_name,"STATS")){
+			gettimeofday(&current, NULL);
+			seconds_running = (double) (current.tv_usec - start.tv_usec) / 1000000 + (double) (current.tv_sec - start.tv_sec);
+			mins_running = seconds_running / 60;
+			seconds_running -= mins_running * 60;
+			if(mins_running > 9)
+				printf("Server up for %d:%f, served %d pages, %d bytes\n", mins_running,seconds_running,pages_served,bytes_served);
+			else
+				printf("Server up for 0%d:%f, served %d pages, %d bytes\n", mins_running,seconds_running,pages_served,bytes_served);
+		}
+		else
+			printf("Unknown command\n");
+		fclose(sock_fp);
 	}
 	
 	return 0;
