1de92680780a5afed2fa5acb62b85f7c3c964609
mayazaha <mayazaha2@gmail.com>
2018-06-02 14:56:19 +0300

    Web server: Bind sockets

--- myhttpd.c
+++ myhttpd.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 int main(int argc,char** argv){
-	int serving_port,command_port,num_threads;
+	int serving_port,command_port,num_threads,serving_sock,command_sock;
 	int i,found = 0;
 	char* root_dir;
+	struct sockaddr_in server,command;
+	struct sockaddr *serverptr,*commandptr;
+	unsigned int serverlen, commandlen;
 	
 	if(argc == 9){
 		for(i=1;i<(argc-1);i++){
@@ -77,7 +84,31 @@ int main(int argc,char** argv){
 		return 1;
 	}
 	
-	printf("Arguments given: Serving port : %d, Command port: %d, Threads: %d, Directory: %s\n",serving_port,command_port,num_threads,root_dir);
+	if ((serving_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+		perror("server socket");
+		exit(-1);
+	}
+	if ((command_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+		perror("command socket");
+		exit(-1);
+	}
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(serving_port);
+	serverptr = (struct sockaddr *) &server;
+	serverlen = sizeof server;
+	command.sin_family = AF_INET;
+	command.sin_addr.s_addr = htonl(INADDR_ANY);
+	command.sin_port = htons(command_port);
+	commandptr = (struct sockaddr *) &command;
+	commandlen = sizeof command;
+	
+	if (bind(serving_sock, serverptr, serverlen) < 0){
+		perror("server bind"); exit(-1);
+	}
+	if (bind(command_sock, commandptr, commandlen) < 0){
+		perror("command bind"); exit(-1);
+	}
 	
 	return 0;
 }
