d5eb66673eda10d290b306ff5c8a798d7ef2fa33
mayazaha <mayazaha2@gmail.com>
2018-06-02 12:24:52 +0300

    Web Server: Check input params

--- myhttpd.c
+++ myhttpd.c
@@ -1,7 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int arcg,char* argv){
-	puts("Hello server");
+int main(int argc,char** argv){
+	int serving_port,command_port,num_threads;
+	int i,found = 0;
+	char* root_dir;
+	
+	if(argc == 9){
+		for(i=1;i<(argc-1);i++){
+			if(!strcmp(argv[i],"-p")){
+				found = 1;
+				if(argv[i+1][0] == '-'){
+					printf("All flags must have a value.\n");
+					return 1;
+				}
+				serving_port = atoi(argv[i+1]);
+				break;
+			}
+		}
+		if(!found){
+			printf("-p flag and its value are required.\n");
+			return 1;
+		}
+		found = 0;
+		for(i=1;i<(argc-1);i++){
+			if(!strcmp(argv[i],"-c")){
+				found = 1;
+				if(argv[i+1][0] == '-'){
+					printf("All flags must have a value.\n");
+					return 1;
+				}
+				command_port = atoi(argv[i+1]);
+				break;
+			}
+		}
+		if(!found){
+			printf("-c flag and its value are required.\n");
+			return 1;
+		}
+		found = 0;
+		for(i=1;i<(argc-1);i++){
+			if(!strcmp(argv[i],"-t")){
+				found = 1;
+				if(argv[i+1][0] == '-'){
+					printf("All flags must have a value.\n");
+					return 1;
+				}
+				num_threads = atoi(argv[i+1]);
+				break;
+			}
+		}
+		if(!found){
+			printf("-t flag and its value are required.\n");
+			return 1;
+		}
+		found = 0;
+		for(i=1;i<(argc-1);i++){
+			if(!strcmp(argv[i],"-d")){
+				found = 1;
+				if(argv[i+1][0] == '-'){
+					printf("All flags must have a value.\n");
+					return 1;
+				}
+				root_dir = argv[i+1];
+				break;
+			}
+		}
+		if(!found){
+			printf("-d flag and its value are required.\n");
+			return 1;
+		}
+	}
+	else{
+		printf("Wrong number of arguments.\n");
+		return 1;
+	}
+	
+	printf("Arguments given: Serving port : %d, Command port: %d, Threads: %d, Directory: %s\n",serving_port,command_port,num_threads,root_dir);
+	
 	return 0;
 }
